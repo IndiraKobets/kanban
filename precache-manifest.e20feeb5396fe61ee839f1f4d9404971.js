26c8c4ef8496ac0da27db09a62d8e13fcf8177f0
indiraaridni <indiraaridni@mail.ru>
2020-06-13 23:36:02 -0400

    Updates

---
+++ precache-manifest.e20feeb5396fe61ee839f1f4d9404971.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e8e9fe74f662578c03cef89b0b323b8",
+    "url": "/kanban/index.html"
+  },
+  {
+    "revision": "b230b474498780505fe3",
+    "url": "/kanban/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1f8d78aa2911472a4206",
+    "url": "/kanban/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b230b474498780505fe3",
+    "url": "/kanban/static/js/2.50e41f0f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/kanban/static/js/2.50e41f0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f8d78aa2911472a4206",
+    "url": "/kanban/static/js/main.8ae49a9d.chunk.js"
+  },
+  {
+    "revision": "0e4ca57cb4a868185634",
+    "url": "/kanban/static/js/runtime-main.395a1b6d.js"
+  }
+]);
